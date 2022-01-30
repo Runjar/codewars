61c5ab8a16358b50dd24f4dda26c318ee14bb404
Runjar <85700889+Runjar@users.noreply.github.com>
2022-01-30 11:10:14 -0500

    8kyu Grasshopper - Variable Assignment Debug

---
+++ python/8kyu/Grasshopper - Variable Assignment Debug.py
@@ -0,0 +1,16 @@
+'''
+Variable assignment
+Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+'''
+
+#My Solution
+a = "dev"
+b = "Lab"
+
+name = a + b
+
+#Best Practice
+a = "dev"
+b = "Lab"
+
+name = a + b
