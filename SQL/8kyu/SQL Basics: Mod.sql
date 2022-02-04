f5d3692b2bbe8bdaac182268b2a69d7bf8e981c5
Runjar <85700889+Runjar@users.noreply.github.com>
2022-02-03 20:06:38 -0500

    8kyu SQL Basics: Mod

---
+++ SQL/8kyu/SQL Basics: Mod.sql
@@ -0,0 +1,19 @@
+/*
+Given the following table 'decimals':
+
+** decimals table schema **
+
+id
+number1
+number2
+Return a table with one column (mod) which is the output of number1 modulus number2.
+*/
+
+
+--My Solution
+SELECT MOD(number1, number2) AS MOD FROM DECIMALS;
+
+--Best Practice
+select
+  mod(number1, number2) as mod
+from decimals
